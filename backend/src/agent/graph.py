f38f9ce36c6aeabd98386cf59f876587d121afd9
JSiggelkow <j.siggelkow@iks-gmbh.com>
2026-06-22 12:34:36 +0200

    add jira agent with humanInTheLoopMiddleware

--- backend/src/agent/graph.py
+++ backend/src/agent/graph.py
@@ -3,12 +3,13 @@ from __future__ import annotations
 from langchain.agents import create_agent
 
 from agent.config import get_model
+from agent.integration import jira_agent
 from agent.subagents.action import database_agent
 from agent.subagents.knowledge import knowledge
 
 graph = create_agent(
         model=get_model(),
-        tools=[knowledge, database_agent],
+        tools=[knowledge, database_agent, jira_agent],
         system_prompt="You are an supervisor Agent with you Tools as Child-Agents"
 )
 
---
+++ backend/src/agent/integration.py
@@ -0,0 +1,60 @@
+import asyncio
+import os
+
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain.agents.middleware import HumanInTheLoopMiddleware
+from langchain_core.tools import tool
+from langchain_mcp_adapters.client import MultiServerMCPClient
+
+from agent.config import get_model
+
+load_dotenv()
+
+
+_client = MultiServerMCPClient(
+    {
+        "atlassian": {
+            "transport": "http",
+            "url": os.environ.get("ATLASSIAN_MCP_URL", "http://localhost:9000/mcp")
+        }
+    }
+)
+
+tools = asyncio.run(_client.get_tools())
+print([t.name for t in tools])
+
+_integration_agent = None
+
+async def _build_integration_agent():
+    tools = await _client.get_tools()
+    return create_agent(
+        name="Integrationsagent",
+        model=get_model(),
+        tools=tools,
+        system_prompt=(
+            "You are an Jira-Assistant. You are connected to a Jira-Project and you can manage it with Tools."
+        ),
+        middleware=[
+            HumanInTheLoopMiddleware(
+                interrupt_on={
+                    "jira_create_issue": True,
+                    "jira_update_issue": True,
+                    "jira_delete_issue": True,
+                    "jira_transition_issue": True,
+                    "jira_search": False,
+                    "jira_get_issue": False,
+                },
+                description_prefix="Freigabe erforderlich für folgende Aktion"
+            )
+        ]
+    )
+
+@tool("jira_agent")
+async def jira_agent(request: str) -> str:
+    """Agent to create, retrieve, or delete issues in the Jira Project"""
+    global _integration_agent
+    if not _integration_agent:
+        _integration_agent = await _build_integration_agent()
+    result = await _integration_agent.ainvoke({"messages": [{"role": "user", "content": request}]})
+    return result["messages"][-1].text
